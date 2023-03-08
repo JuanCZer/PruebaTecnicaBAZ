ca9c2311d5c06fcb2fe5da29b0eab3b789bbef36
JuanCZer <you@example.com>
2023-03-07 22:46:39 -0600

    Agregar archivos de proyecto.

---
+++ PruebaTecnicaBAZ/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using PruebaTecnicaBAZ.Rest;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PruebaTecnicaBAZ
+{
+    /// <summary>
+    /// Lógica de interacción para App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static MainWindow VentanaPrincipal;
+        public static RestServicio restServico;
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            VentanaPrincipal = new MainWindow();
+            restServico = new RestServicio();
+
+            VentanaPrincipal.Show();
+        }
+
+        public async static void ImprimirConsulta()
+        {
+            string cadenaObjetos = await restServico.GetResultAPI();
+
+            VentanaPrincipal.DespligueInformacion.Text = cadenaObjetos;
+        }
+    }
+}
---
+++ PruebaTecnicaBAZ/Rest/RestServicio.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using PruebaTecnicaBAZ.Modelos;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaTecnicaBAZ.Rest
+{
+    public class RestServicio
+    {
+        public async Task<string> GetResultAPI()
+        {
+            string cadenaOrden = "";
+            var client = new RestClient("https://api.github.com");
+            var request = new RestRequest("users", Method.GET);
+            request.AddHeader("Content-Type", "application/json");
+
+            IRestResponse<List<ResponseAPI>> response = client.Execute<List<ResponseAPI>>(request);
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                List<ResponseAPI> users = response.Data;
+
+                int contadorLimitante = 0;
+                foreach (ResponseAPI user in users)
+                {
+                    if (contadorLimitante > 9) break;
+                    cadenaOrden += $"{user.id}|{user.login}\r\n";
+                    contadorLimitante++;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Peticion fallida: " + response.StatusCode);
+            }
+
+            return cadenaOrden;
+        }
+    }
+}
